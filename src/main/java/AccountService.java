b11efd3ed40a6bbe156fb022e7ea266aea196ce0
Framsida <Joshuastait@gmail.com>
2018-08-23 10:58:01 +0100

    Improve AccountService counter

--- src/main/java/AccountService.java
+++ src/main/java/AccountService.java
@@ -3,14 +3,15 @@ import java.util.Map;
 
 public class AccountService {
     private Map<Integer, Account> accountMap;
+    private int counter = 1;
 
     public AccountService() {
         accountMap = new HashMap<Integer, Account>();
     }
 
     public int addAccountToMap(Account accountToAdd) {
-        this.accountMap.put(this.accountMap.size()+1, accountToAdd);
-        return this.accountMap.size();
+        this.accountMap.put(counter, accountToAdd);
+        return counter++;
     }
 
     public void getAccountById(int idOfAccount) {
